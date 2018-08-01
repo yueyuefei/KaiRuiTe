4fb5a90c4ef4eb97c255fd84f3700e90512004b0
yueyuefei <Administrator@ZYX>
2018-08-01 14:23:50 +0800

    1

--- src/main/java/com/kairuite/web/controller/IndexController.java
+++ src/main/java/com/kairuite/web/controller/IndexController.java
@@ -12,5 +12,71 @@ public class IndexController {
     public String index(ModelMap mmap) {
     	return "index";
     }
+    
+    // 产品中心
+    @GetMapping("/product")
+    public String product(ModelMap mmap) {
+    	return "product";
+    }
+    
+    // 产品中心-产品介绍
+    @GetMapping("/Product_into")
+    public String Product_into(ModelMap mmap) {
+    	return "Product_into";
+    }
+    
+    // 彩印纸箱
+    @GetMapping("/Product_into1")
+    public String Product_into1(ModelMap mmap) {
+    	return "Product_into1";
+    }
+    
+    // 牛皮纸箱
+    @GetMapping("/Product_into2")
+    public String Product_into2(ModelMap mmap) {
+    	return "Product_into2";
+    }
+    
+    // 瓦楞纸
+    @GetMapping("/Product_into3")
+    public String Product_into3(ModelMap mmap) {
+    	return "Product_into3";
+    }
+    
+    // 定制纸箱
+    @GetMapping("/Product_into4")
+    public String Product_into4(ModelMap mmap) {
+    	return "Product_into4";
+    }
+    
+    // 产品五
+    @GetMapping("/Product_into5")
+    public String Product_into5(ModelMap mmap) {
+    	return "Product_into5";
+    }
+    
+    // 客户案例
+    @GetMapping("/Case")
+    public String Case(ModelMap mmap) {
+    	return "Case";
+    }
+    
+    //公司动态
+    @GetMapping("/dynamic")
+    public String dynamic(ModelMap mmap) {
+    	return "dynamic";
+    }
+    
+    //公司简介
+    @GetMapping("/Company")
+    public String Company(ModelMap mmap) {
+    	return "Company";
+    }
+    
+    //联系方式
+    @GetMapping("/Contact")
+    public String Contact(ModelMap mmap) {
+    	return "Contact";
+    }
 
 }
