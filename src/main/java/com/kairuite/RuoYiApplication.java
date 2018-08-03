bc4346bed0d5254e3671db6afd8bd71432b405c3
yueyuefei <Administrator@windows10.microdone.cn>
2018-08-03 16:23:52 +0800

    修改

--- src/main/java/com/kairuite/RuoYiApplication.java
+++ src/main/java/com/kairuite/RuoYiApplication.java
@@ -1,5 +1,7 @@
 package com.kairuite;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -16,5 +18,12 @@ public class RuoYiApplication
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  启动成功   ლ(´ڡ`ლ)ﾞ  ");
+        //自动打开首页
+//        try {
+//			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler http://localhost");
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
     }
 }
